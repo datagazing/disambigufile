647a079736c3480d1b2d215dcbabb1bcc52ffccb
Brendan Strejcek <brendan@datagazing.com>
2021-06-02 12:45:39 -0400

    initial commit

---
+++ disambigufile/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for disambigufile."""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.1.0'
---
+++ tests/test_disambigufile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `disambigufile` package."""
+
+
+import unittest
+
+from disambigufile import disambigufile
+
+
+class TestDisambigufile(unittest.TestCase):
+    """Tests for `disambigufile` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
