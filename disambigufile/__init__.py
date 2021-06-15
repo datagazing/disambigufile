deb26e42d650eb4c444f23ae2f030eee204aa04e
Brendan Strejcek <brendan@datagazing.com>
2021-06-15 18:29:17 -0400

    Bump version: 0.1.0 → 0.2.0

--- disambigufile/__init__.py
+++ disambigufile/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .disambigufile import DisFile, Error, NoMatchError, AmbiguousMatchError
 
